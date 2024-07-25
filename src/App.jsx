50d2fc6031f67d4a50f068260f763f17fb2c7927
Floresa Zogaj <floresaa.zogaj@gmail.com>
2024-07-25 14:05:04 +0200

    React Essentials

--- src/App.jsx
+++ src/App.jsx
@@ -41,8 +41,8 @@ function App() {
           <h2>Examples</h2>
           <menu>
             <TabButton isSelected={selectedTopic ==='components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton  isSelected={selectedTopic ==='jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton   isSelected={selectedTopic ==='props'}  onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic ==='jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic ==='props'}  onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton isSelected={selectedTopic ==='state'}   onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
           {tabContent}
